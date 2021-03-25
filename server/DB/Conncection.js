e266a1ee9ad39642e4e974ad4c88f75623db7b19
Sanketjn18 <53268867+Sanketjn18@users.noreply.github.com>
2021-03-25 15:15:03 +0530

    Update Conncection.js

--- server/DB/Conncection.js
+++ server/DB/Conncection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI ="mongodb+srv://dbuser:Sanket@007@cluster0.ggccd.mongodb.net/USER_DETAILS?retryWrites=true&w=majority";
+const URI ="mongodb+srv://dbuser:dbuser@cluster0.ggccd.mongodb.net/USER_DETAILS?retryWrites=true&w=majority";
 
 const connectDB = async () => {
   await mongoose.connect(URI, {
